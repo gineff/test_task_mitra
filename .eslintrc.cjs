7f47d3e9f22040ea8bd18b30b26651d2e6e8d548
gineff <canone@inbox.ru>
2023-05-20 15:08:40 +0300

    chore: add initial settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,27 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/ban-ts-comment': 1,
+    'no-global-assign': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+  publicDir: './src/assets',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
