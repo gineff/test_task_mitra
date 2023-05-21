ec384aeff10cb9fd28ec1d659c0694435e83b96b
gineff <canone@inbox.ru>
2023-05-21 12:44:01 +0300

    ref: add back button to Profile Page

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,7 +1,22 @@
 import { DefaultPageTmpl } from '@components/DefaultPageTmpl'
+import { Navbar } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import './ProfilePage.scss'
 
 const ProfilePage = () => {
-  return <DefaultPageTmpl>Profile</DefaultPageTmpl>
+  const navigate = useNavigate()
+  const goBack = () => navigate(-1)
+  return (
+    <DefaultPageTmpl showNav={false} className="profile-page">
+      <Navbar expand={false} variant="light" bg="light" className="app-header">
+        <Navbar.Toggle
+          onClick={() => goBack()}
+          className="post-page__button-back"
+        />
+      </Navbar>
+      Profile
+    </DefaultPageTmpl>
+  )
 }
 
 export { ProfilePage }
