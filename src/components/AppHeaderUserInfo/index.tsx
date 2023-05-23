9cd16c06e2a4b1fa287c0765a99db40122800807
gineff <canone@inbox.ru>
2023-05-23 12:02:22 +0300

    style: change user profile width (components)

--- src/components/AppHeaderUserInfo/index.tsx
+++ src/components/AppHeaderUserInfo/index.tsx
@@ -1,15 +1,23 @@
 import { FC, HTMLAttributes } from 'react'
 import { Form, Row } from 'react-bootstrap'
+import classnames from 'classnames'
+import './AppHeaderUserInfo.scss'
 
 type UserInfoProps = HTMLAttributes<HTMLElement> & {
   name: string
   email: string
+  className?: string
 }
 
-const AppHeaderUserInfo: FC<UserInfoProps> = ({ name, email }) => {
+const AppHeaderUserInfo: FC<UserInfoProps> = ({
+  name,
+  email,
+  className: cls,
+  ...attr
+}) => {
   return (
     <>
-      <div className="user-info ms-4 h-100">
+      <div className={classnames('user-info ms-4 h-100 w-50', cls)} {...attr}>
         <Row className="user-info__name flex-nowrap">
           <Form.Label>name:</Form.Label>
           <span className="d-inline-flex fs-6 fw-bold">{name}</span>
