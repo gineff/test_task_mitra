f360547bf249a84def58df0987a30770d741755d
gineff <canone@inbox.ru>
2023-05-23 15:30:39 +0300

    ft: add delay (api)

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosResponse } from 'axios'
 import { Post } from '@constants/posts'
+import delay from '@utils/delay'
 
 export const fetchPosts = async () => {
   const response: AxiosResponse<Post[]> = await axios.get(
     `https://jsonplaceholder.typicode.com/posts`
   )
+  await delay(500)
   return response.data
 }
 
--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosResponse } from 'axios'
 import { User } from '@constants/posts'
+import delay from '@utils/delay'
 
 export const fetchUsers = async () => {
   const response: AxiosResponse<User[]> = await axios.get(
     `https://jsonplaceholder.typicode.com/users`
   )
+  await delay(500)
   return response.data
 }
 
