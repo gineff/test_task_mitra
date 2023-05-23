c162e28b3ab9ce3f548a5c0887f514d50eaebfa8
gineff <canone@inbox.ru>
2023-05-23 12:10:32 +0300

    ft: change withTemplate 2nd prop (components)

--- src/pages/PostsPage/index.tsx
+++ src/pages/PostsPage/index.tsx
@@ -6,36 +6,34 @@ import { withTemplate } from '@hoc/withTemplate'
 import { useEffect, useMemo } from 'react'
 import { Spinner } from 'react-bootstrap'
 import { PostsList } from '@components/PostsList'
-import { Post as PostType } from '@constants/posts'
 
-const PostsPage = withTemplate((): JSX.Element => {
-  const dispatch = useAppDispatch()
-  const { loadingStatus, data: posts } = useAppSelector(selectPosts)
-  const isLoading = useMemo(
-    () => loadingStatus === LoadingStatus.Loading,
-    [loadingStatus]
-  )
+const PostsPage = withTemplate(
+  (): JSX.Element => {
+    const dispatch = useAppDispatch()
+    const { loadingStatus, data: posts } = useAppSelector(selectPosts)
+    const isLoading = useMemo(
+      () => loadingStatus === LoadingStatus.Loading,
+      [loadingStatus]
+    )
 
-  useEffect(() => {
-    const isPostsEmpty = !posts.length
-    if (isPostsEmpty) {
-      dispatch(fetchPosts())
+    useEffect(() => {
+      const isPostsEmpty = !posts.length
+      if (isPostsEmpty) {
+        dispatch(fetchPosts())
+      }
+    }, [posts])
+
+    if (isLoading) {
+      return <Spinner animation="grow" variant="light" className="m-4 p-4" />
     }
-  }, [posts])
 
-  if (isLoading) {
-    return <Spinner animation="grow" variant="light" className="m-4 p-4" />
-  }
+    if (loadingStatus === LoadingStatus.Failed) {
+      return <span>Ошибка загрузки</span>
+    }
 
-  if (loadingStatus === LoadingStatus.Failed) {
-    return <span>Ошибка загрузки</span>
-  }
-
-  return (
-    <div className="post-page__posts p-4">
-      <PostsList posts={posts} />
-    </div>
-  )
-})
+    return <PostsList className="p-4" posts={posts} />
+  },
+  { className: 'post-page' }
+)
 
 export { PostsPage }
