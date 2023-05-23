accbc8bbbc26c985689cf52b08027c95475b3ec7
gineff <canone@inbox.ru>
2023-05-23 12:05:47 +0300

    ft: add prop:showAvatar to posts-list (components)

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -1,19 +1,25 @@
 import { Post as PostType } from '@constants/posts'
 import { FC, HTMLAttributes } from 'react'
 import { Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './PostItem.scss'
 
-type PostProps = Omit<HTMLAttributes<HTMLElement>, 'id'> & PostType
+type PostProps = Omit<HTMLAttributes<HTMLElement>, 'id'> &
+  PostType & { showAvatar: boolean }
 
-const PostItem: FC<PostProps> = ({ title, body }) => {
+const PostItem: FC<PostProps> = ({ title, body, userId, showAvatar }) => {
   return (
     <Card className="post my-3">
-      <Card.Header className="post__header">
-        <Card.Img
-          src="https://cdn-icons-png.flaticon.com/128/4140/4140048.png"
-          className="post__avatar"
-        />
-      </Card.Header>
+      {showAvatar ? (
+        <Card.Header className="post__header">
+          <Link to={`/profile/${userId}`}>
+            <Card.Img
+              src="https://cdn-icons-png.flaticon.com/128/4140/4140048.png"
+              className="post__avatar"
+            />
+          </Link>
+        </Card.Header>
+      ) : null}
       <Card.Body className="post__body">
         <Card.Title>{title}</Card.Title>
         <Card.Text>{body}</Card.Text>
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -4,15 +4,16 @@ import { Post as PostType } from '@constants/posts'
 
 type PostItemProps = HTMLAttributes<HTMLElement> & {
   posts: PostType[]
+  showAvatar?: boolean
 }
 
-const PostsList: FC<PostItemProps> = ({ posts }) => {
+const PostsList: FC<PostItemProps> = ({ posts, showAvatar = true }) => {
   return (
-    <>
+    <div className="posts-list p-2">
       {posts.map((post: PostType) => (
-        <PostItem key={post.id} {...post} />
+        <PostItem key={post.id} showAvatar={showAvatar} {...post} />
       ))}
-    </>
+    </div>
   )
 }
 
